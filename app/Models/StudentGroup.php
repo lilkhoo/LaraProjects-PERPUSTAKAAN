6c9f161aa5e4510196a463798366a7c2142e45bf
ekoongt123 <setyonobowo24@gmail.com>
2021-11-19 08:03:22 +0700

    Membuat project Perpustakaan

---
+++ app/Models/StudentGroup.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StudentGroup extends Model
+{
+    use HasFactory;
+
+    protected $table = 'student_groups';
+    protected $fillable = ['rombel'];
+}
